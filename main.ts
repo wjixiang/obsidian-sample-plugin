56b88f77f1346659f0fda9574d8307b75025c263
wjixiang <123081421+wjixiang@users.noreply.github.com>
2023-10-28 23:45:42 +0800

    first commit

--- main.ts
+++ main.ts
@@ -1,134 +1,394 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { ItemView, MarkdownRenderer, WorkspaceLeaf } from "obsidian";
+import { Plugin,FileManager} from "obsidian";
+import { Result, getAPI } from "obsidian-dataview";
 
-// Remember to rename these classes and interfaces!
+const api = getAPI();
+var answer = "NONE"
+var filesWithTag = tag();
+var select_t = getRandomElements(filesWithTag,1)
+var timu = select_t[0]
+var clas_t = []
+var select_class = "病生"
 
-interface MyPluginSettings {
-	mySetting: string;
+
+function getRandomElements(arr,n) {
+    let result = [];
+    let len = arr.length;
+    let taken = new Array(len);
+    if (n > len)
+        throw new RangeError("getRandomElements: more elements taken than available");
+    while (n--) {
+        let x = Math.floor(Math.random() * len);
+        result.push(arr[x in taken ? taken[x] : x]);
+        taken[x] = --len in taken ? taken[len] : len;
+    }
+    return result;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+function weight(filelist){ //返回安权重抽取的索引值
+	let weight_list = [];
+	let totalWeight = 0
+	for(var i=0;i<filelist.length;i++){
+		let wei_a = this.app.metadataCache.getFileCache(filelist[i]).frontmatter.times;
+		let wei_b = this.app.metadataCache.getFileCache(filelist[i]).frontmatter.link;
+		totalWeight = totalWeight + wei_b/wei_a
+		weight_list.push(wei_b/wei_a)
+	}
+
+	let randomNum = Math.random() * totalWeight;
+	let weightSum = 0;
+
+	for (let i = 0; i < weight_list.length; i++) {
+		weightSum += weight_list[i];
+		if (randomNum <= weightSum) {
+		  return i;
+		}
+	  }
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+function tag(){
+	const tagName = '错题'; // 你指定的标签
+	const markdownFiles = this.app.vault.getMarkdownFiles(); // 获取所有 Markdown 文件
+	const filesWithTag = [];
 
-	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+	for (let file of markdownFiles) {
+		const cache = this.app.metadataCache.getFileCache(file); // 获取文件缓存
+		if (cache && cache.tags) { // 检查文件是否有标签
+			for (let tag of cache.tags) {
+				if (tag.tag.includes(`#${tagName}`)) { // 检查标签是否包含你指定的标签
+					filesWithTag.push(file);
+					break;
 				}
 			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		}
 	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+	return filesWithTag	
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
+function classify(flist,clas){
+	clas_t = []
+	flist.forEach(note => {
+		let properties = this.app.metadataCache.getFileCache(note).frontmatter; // 获取笔记的元数据
+		if (properties && properties.class === clas) {
+			// 这里是找到了一个符合条件的笔记，你可以根据需要进行操作
+			//console.log(note.basename);
+			clas_t.push(note)
+		}
+	});
+	return clas_t;
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+export const VIEW_TYPE_EXAMPLE = "example-view";
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
+export class ExampleView extends ItemView { //此处为创建一个view
+  constructor(leaf: WorkspaceLeaf) {
+    super(leaf);
+  }
+
+  getViewType() {
+    return VIEW_TYPE_EXAMPLE;
+  }
+
+  getDisplayText() {
+    return "BrushTi";
+  }
+
+  async onOpen() {
+    const container = this.containerEl.children[1];
+    container.empty();
+    container.createEl("h1", { text: "wjx" });
+
+	// 创建一个输入框和一个显示区域
+	container.innerHTML = `
+	<head>
+		<style>
+			button {
+				font-size: 16px;
+				border: none;
+				border-radius: 5px;
+				color: white;
+			}
+			#tn {
+				background-color: #4CAF50;
+			}
+			#dn {
+				background-color: #008CBA;
+			}
+			#tz {
+				background-color: #f44336;
+			}
+			#t, #f {
+				background-color: #555555;
+			}
+			p {
+				padding: 15px;
+				color: #87CEEB;
+				border-radius: 5px;
+				box-shadow: 0 2px 5px rgba(0,0,0,0.3);
+			}
+			.button-container {
+				display: flex;
+				justify-content: space-between;
+				width: 100%;
+			  }
+		</style>
+	</head>
+	<body>
+		<input type="text" id="class">
+		</br>
+		<button id="tn">抽题</button>
+		<button id="tz">跳转</button>
+		<br/>
+		<p id="note-content"></p>
+		<p id="note-answer"></p>
+		<div class="button-container">
+		<button id="dd">✅</button>
+		<button id="dn">答案</button>
+		<button id="cc">❌</button>
+	  </div>
+		<div id="markdown-output"></div>
+	</body>
+	
+	`;
+
+	// 添加事件监听器来处理按钮点击
+	// const button = container.querySelector("#load-note");
+	// button.addEventListener("click", async () => {
+	// 	const noteName = container.querySelector("#note-path").value;
+	// 	const note = this.app.vault.getAbstractFileByPath(noteName);//注意需要根据路径来打开笔记
+	// 	const content = this.app.vault.read(note);
+	// 	//const markdownRendered = await this.app.markdownRenderer.render(content);
+	// 	//container.querySelector("#markdown-output").innerHTML = markdownRendered;
+
+	// 	this.app.vault.read(note).then(content => {
+	// 		container.querySelector("#note-content").textContent = content;//这段代码可以获得笔记的内容后，将其输出到note-content框中
+	// 		console.log(content)
+	// 		var text = content.replace(/(\n|\r|\r\n|\u21b5)/g, '<br/>');
+	// 		container.querySelector("#note-content").innerHTML = text
+	// 	});
+	// 	this.app.workspace.openLinkText(note.basename, note.path, true);
+	// });
+
+	const button2 = container.querySelector("#tn");
+	button2.addEventListener("click", async () => {
+		select_class = container.querySelector("#class").value;
+		filesWithTag = classify(tag(),select_class); //获得指定科目的所有题目
+		//select_t = getRandomElements(filesWithTag,1)
+
+		timu = filesWithTag[weight(filesWithTag)]
+
+		//timu = select_t[0] //根据权重进行抽选
+
+		var content = this.app.vault.read(timu).then(content => {
+			// 假设 markdown 是你的 Markdown 字符串
+			let markdown = content;
+			// 使用正则表达式匹配 ad-question 代码块
+			let match = markdown.match(/```ad-question([^`]+)```/);
+			let codeBlock = match[1];
+			// 分割代码块为行
+			let lines = codeBlock.split('\n');
+			// 移除 "title" 行
+			lines = lines.filter(line => !line.startsWith('title:'));
+			// 合并剩余的行
+			var result = lines.join('\n');
+			//console.log(result); // 输出结果
+			var text = result.replace(/(\n|\r|\r\n|\u21b5)/g, '<br/>');
+			container.querySelector("#note-content").innerHTML = text
+		});
+		// console.log(select_t[0])
+		var answe = this.app.vault.read(select_t[0]).then(content => {
+			// 假设 markdown 是你的 Markdown 字符串
+			let markdown = content;
+			// 使用正则表达式匹配 ad-question 代码块
+			let match = markdown.match(/```ad-note([^`]+)```/);
+			let codeBlock = match[1];
+			// 分割代码块为行
+			let lines = codeBlock.split('\n');
+			// 移除 "title" 行
+			lines = lines.filter(line => !line.startsWith('collapse:'));
+			lines = lines.filter(line => !line.startsWith('title:'));
+			// 合并剩余的行
+			var result = lines.join('\n');
+			//console.log(result); // 输出结果
+			answer = result.replace(/(\n|\r|\r\n|\u21b5)/g, '<br/>');
+			// container.querySelector("#note-content").innerHTML = text
+		});
+		container.querySelector("#note-answer").innerHTML = ""
+	});
+
+	const button3 = container.querySelector("#dn");
+	button3.addEventListener("click", async () => {
+		container.querySelector("#note-answer").innerHTML = answer
+	});
+
+	const button4 = container.querySelector("#tz");
+	button4.addEventListener("click", async () => {
+		this.app.workspace.openLinkText(timu.basename,timu.path, true);
+	});
+
+	const button5 = container.querySelector("#dd");
+	button5.addEventListener("click", async () => {
+		let cache = this.app.metadataCache.getFileCache(timu)
+		let wrong_times = cache.frontmatter.link
+		let sum_times = cache.frontmatter.times
+		console.log(wrong_times)
+		//cache.frontmatter.times = String(2)
+		//timu.vault.modify(timu,cache.frontmatter);
+		//timu.file.saveFrontmatter(cache.frontmatter)
+		this.app.fileManager.processFrontMatter(timu, (frontmatter) => {
+			frontmatter.times = sum_times+1; // 修改元数据
+			return frontmatter;
+		});
+		select_class = container.querySelector("#class").value;
+		filesWithTag = classify(tag(),select_class); //获得指定科目的所有题目
+		//select_t = getRandomElements(filesWithTag,1)
+
+		timu = filesWithTag[weight(filesWithTag)]
+
+		//timu = select_t[0] //根据权重进行抽选
+
+		var content = this.app.vault.read(timu).then(content => {
+			// 假设 markdown 是你的 Markdown 字符串
+			let markdown = content;
+			// 使用正则表达式匹配 ad-question 代码块
+			let match = markdown.match(/```ad-question([^`]+)```/);
+			let codeBlock = match[1];
+			// 分割代码块为行
+			let lines = codeBlock.split('\n');
+			// 移除 "title" 行
+			lines = lines.filter(line => !line.startsWith('title:'));
+			// 合并剩余的行
+			var result = lines.join('\n');
+			//console.log(result); // 输出结果
+			var text = result.replace(/(\n|\r|\r\n|\u21b5)/g, '<br/>');
+			container.querySelector("#note-content").innerHTML = text
+		});
+		// console.log(select_t[0])
+		var answe = this.app.vault.read(select_t[0]).then(content => {
+			// 假设 markdown 是你的 Markdown 字符串
+			let markdown = content;
+			// 使用正则表达式匹配 ad-question 代码块
+			let match = markdown.match(/```ad-note([^`]+)```/);
+			let codeBlock = match[1];
+			// 分割代码块为行
+			let lines = codeBlock.split('\n');
+			// 移除 "title" 行
+			lines = lines.filter(line => !line.startsWith('collapse:'));
+			lines = lines.filter(line => !line.startsWith('title:'));
+			// 合并剩余的行
+			var result = lines.join('\n');
+			//console.log(result); // 输出结果
+			answer = result.replace(/(\n|\r|\r\n|\u21b5)/g, '<br/>');
+			// container.querySelector("#note-content").innerHTML = text
+		});
+		container.querySelector("#note-answer").innerHTML = ""
+	});
+
+	const button6 = container.querySelector("#cc");
+	button6.addEventListener("click", async () => {
+		let cache = this.app.metadataCache.getFileCache(timu)
+		let wrong_times = cache.frontmatter.link
+		let sum_times = cache.frontmatter.times
+		console.log(wrong_times)
+		//cache.frontmatter.times = String(2)
+		//timu.vault.modify(timu,cache.frontmatter);
+		//timu.file.saveFrontmatter(cache.frontmatter)
+		this.app.fileManager.processFrontMatter(timu, (frontmatter) => {
+			frontmatter.times = sum_times+1; // 修改元数据
+			frontmatter.link = wrong_times+1;
+			return frontmatter;
+		});
+		select_class = container.querySelector("#class").value;
+		filesWithTag = classify(tag(),select_class); //获得指定科目的所有题目
+		//select_t = getRandomElements(filesWithTag,1)
+
+		timu = filesWithTag[weight(filesWithTag)]
+
+		//timu = select_t[0] //根据权重进行抽选
+
+		var content = this.app.vault.read(timu).then(content => {
+			// 假设 markdown 是你的 Markdown 字符串
+			let markdown = content;
+			// 使用正则表达式匹配 ad-question 代码块
+			let match = markdown.match(/```ad-question([^`]+)```/);
+			let codeBlock = match[1];
+			// 分割代码块为行
+			let lines = codeBlock.split('\n');
+			// 移除 "title" 行
+			lines = lines.filter(line => !line.startsWith('title:'));
+			// 合并剩余的行
+			var result = lines.join('\n');
+			//console.log(result); // 输出结果
+			var text = result.replace(/(\n|\r|\r\n|\u21b5)/g, '<br/>');
+			container.querySelector("#note-content").innerHTML = text
+		});
+		// console.log(select_t[0])
+		var answe = this.app.vault.read(select_t[0]).then(content => {
+			// 假设 markdown 是你的 Markdown 字符串
+			let markdown = content;
+			// 使用正则表达式匹配 ad-question 代码块
+			let match = markdown.match(/```ad-note([^`]+)```/);
+			let codeBlock = match[1];
+			// 分割代码块为行
+			let lines = codeBlock.split('\n');
+			// 移除 "title" 行
+			lines = lines.filter(line => !line.startsWith('collapse:'));
+			lines = lines.filter(line => !line.startsWith('title:'));
+			// 合并剩余的行
+			var result = lines.join('\n');
+			//console.log(result); // 输出结果
+			answer = result.replace(/(\n|\r|\r\n|\u21b5)/g, '<br/>');
+			// container.querySelector("#note-content").innerHTML = text
+		});
+		container.querySelector("#note-answer").innerHTML = ""
+	});
+  }
+
+  async onClose() {
+    // Nothing to clean up.
+  }
+}
+
+export default class ExamplePlugin extends Plugin { //此处为启动设置
+  async onload() {
+	//this.app.emulateMobile(false);
+    this.registerView(//注册视图
+      VIEW_TYPE_EXAMPLE,
+      (leaf) => new ExampleView(leaf)
+    );
+
+    this.addRibbonIcon("dice", "Activate view", () => {
+      this.activateView();
+	  console.log("start")
+
+	//   this.app.workspace.getLeaf().setViewState({
+	// 	type: VIEW_TYPE_EXAMPLE,
+	// 	active: true,
+	//   });
+    });
+
+  }
+
+  async onunload() {
+  }
+
+  async activateView() {
+	let { workspace }  = this.app;
+
+	let leaf: WorkspaceLeaf | null = null;
+	let leaves = workspace.getLeavesOfType(VIEW_TYPE_EXAMPLE);
+
+	if (leaves.length > 0) {
+		// A leaf with our view already exists, use that
+		leaf = leaves[0];
+	} else {
+		// Our view could not be found in the workspace, create a new leaf
+		// in the right sidebar for it
+		let leaf = workspace.getRightLeaf(false);
+		await leaf.setViewState({ type: VIEW_TYPE_EXAMPLE, active: true });
 	}
 
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+	// "Reveal" the leaf in case it is in a collapsed sidebar
+    workspace.revealLeaf(leaf);
+  }
 }
